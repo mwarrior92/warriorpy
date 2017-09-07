bf226ba12b85df48b7243a2b42eba0a71fc189f7
Marc Anthony Warrior <marc.warrior@gmail.com>
2017-09-07 15:01:04 -0500

    finished ripe/systems.py, but I still need to test it

--- ripe/systems.py
+++ ripe/systems.py
@@ -6,6 +6,7 @@ from functools import wraps
 import time
 import ripe_api as ra
 import threading
+from warriorpy.shorthand import looplist
 
 ##################################################################
 #                           DESCRIPTION
@@ -63,7 +64,23 @@ plotsdir = df.rightdir(basedir+"plots/")
 # wrapper to manage experiment (high level)
 
 class ripe_experiment:
-    def __init__(self, probe_list, meas_seq, reps, max_threads=1):
+    def __init__(self, probe_list, meas_seq, reps, max_threads=1,
+            init_kwargs=[None]):
+        '''
+        :param probe_list: list of probe IDs (int) for experiment
+        :param meas_seq: (ordered) list of functions to perform on each probe.
+        The kwargs input of each successive function should be the output of its
+        predecessor.
+        :param reps: the number of experiments to run on each probe
+        :max threads: the number of experiments to run in parallel
+        :init_kwargs: a list of kwargs (dicts) for the first function in
+        meas_seq, such that the nth rep gets the nth kwargs from init_kwargs
+        as input for its 0th function. If len(init_kwargs) < reps, the
+        init_kwargs will modular repeat
+
+        data from the experiment is stored in self.meas_data in the following
+        format: {meas_seq step: {probe id: [0th rep, 1st rep, ..., nth rep]}}
+        '''
         self.probe_list = probe_list
         self.meas_seq = meas_seq
         self.meas_state = list()
@@ -72,6 +89,10 @@ class ripe_experiment:
         self.meas_state[0] = set(probe_list)
         self.sem = threading.Semaphore(max_threads)
         self.lv = threading.Lock()
+        self.init_kwargs = looplist.looplist(init_kwargs)
+        if len(self.init_kwargs) == 0:
+            self.init_kwargs.append(None)
+
 
     def run(self):
         reps = 0
@@ -80,10 +101,20 @@ class ripe_experiment:
             for probe in self.probe_list:
                 self.sem.acquire()
                 threading.Thread(target=self.run_meas_seq, args=(self),
-                        kwargs={'probe':probe})
+                        kwargs={'probe':probe, 'kwargs':init_kwargs[reps]})
 
 
-    def run_meas_seq(self, probe):
+    def run_sets(self):
+        reps = 0
+        steps = len(self.meas_state)
+        for probe in self.probe_list:
+            while reps < self.max_reps:
+                self.sem.acquire()
+                threading.Thread(target=self.run_meas_seq, args=(self),
+                        kwargs={'probe':probe, 'kwargs':init_kwargs[reps]})
+
+
+    def run_meas_seq(self, probe, kwargs):
         for i, meas_func in enumerate(self.meas_seq):
             step = 'step_'+str(i)
             if i == 0:
@@ -93,8 +124,11 @@ class ripe_experiment:
                 data = meas_func(probe, kwargs**)
             self.lv.acquire()
             # dump data to self.meas_data
-            if probe not in self.meas_data:
-                pass
+            if step not in self.meas_data:
+                self.meas_data[step] = dict()
+            if probe not in self.meas_data[step]:
+                self.meas_data[step][probe] = list()
+            self.meas_data[step][probe].append(data)
             self.lv.release()
             if data is None:
                 break
